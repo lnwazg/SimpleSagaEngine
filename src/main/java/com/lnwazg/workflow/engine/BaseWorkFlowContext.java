7b44979d6a739aa5c23ce79ca28ccc64c5265163
lnwazg <lnwazg@126.com>
2021-08-14 23:03:58 +0800

    添加事务Id

--- src/main/java/com/lnwazg/workflow/engine/BaseWorkFlowContext.java
+++ src/main/java/com/lnwazg/workflow/engine/BaseWorkFlowContext.java
@@ -2,11 +2,17 @@ package com.lnwazg.workflow.engine;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 /**
  * 基础的工作流上下文
  */
 @Data
 public class BaseWorkFlowContext {
+    /**
+     * 当前的事务Id
+     */
+    private String txId = UUID.randomUUID().toString();
     /**
      * 下一个要处理节点的名称
      */
